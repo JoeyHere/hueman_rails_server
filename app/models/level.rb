5cdd38e1c22f066f85e727ba87640e5e32eb382e
JoeyHere <josephodell@outlook.com>
2019-04-08 19:32:50 +0100

    end of day 8

--- app/models/level.rb
+++ app/models/level.rb
@@ -2,19 +2,19 @@ class Level < ApplicationRecord
     belongs_to :user
     has_many :user_level_actions
 
-    def completed(userId) 
-        @user = User.find_by(id: userId)
-        completed = @user.user_level_actions.select(|action| 
-            {
-               if action.completed && action.level_id == self.id
-                    return true
-                else 
-                   return false
-                end
-            }
-            )
-        end
-    end
+    # def completed(userId) 
+    #     @user = User.find_by(id: userId)
+    #     completed = @user.user_level_actions.select(|action| 
+    #         {
+    #            if action.completed && action.level_id == self.id
+    #                 return true
+    #             else 
+    #                return false
+    #             end
+    #         }
+    #         )
+    #     end
+    # end
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
     has_secure_password
     has_many :levels
     has_many :user_level_actions
+
+   def completedLevelIds
+        actions = self.user_level_actions.select{|action|  action.completed}
+        return actions.map{ |ac| ac.level_id }
+   end
+
 end
+
