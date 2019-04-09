3161020a544726e94d6f7615bfc68280a79c2bca
JoeyHere <josephodell@outlook.com>
2019-04-09 23:49:47 +0100

    end of day commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
     has_many :levels
     has_many :user_level_actions, dependent: :destroy
 
+    validates :user_name, uniqueness: { case_sensitive: false }
+
    def completedLevelIds
         actions = self.user_level_actions.select{|action|  action.completed}
         return actions.map{ |ac| ac.level_id }
